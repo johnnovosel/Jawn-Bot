5e0ee07a6b5d837002012ff6f503bc6cfcd0928a
johnnovosel <johnnovosel96@gmail.com>
2022-01-31 22:26:12 -0600

    f

--- Summoner.js
+++ Summoner.js
@@ -12,7 +12,7 @@ export default class Summoner {
         try {
             const response = await fetch('https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/' + this.puuid +  '/ids?start=0&count=1', {
             headers: {
-                "X-Riot-Token": "RGAPI-612d80c7-f928-4805-82f3-abb22aa04d23"
+                "X-Riot-Token": ""
             }
         })
         data = await response.json();
--- index.js
+++ index.js
@@ -15,12 +15,8 @@ client.on("ready", async () => {
   var summoner = new Summoner("umkbzSZkpZfXuLtwvnwEtPSOzbQkmJTMq7q8-uCFTDrtALaU0AKElqwugPZLuONYpLAt4rBYJT7_KA")
   await summoner.getLastestGame();
   console.log(summoner.latestGame)
-  //getUser();
-});
 
-// client.on("interactionCreate", (interaction: Interaction) => {
-//   client.executeInteraction(interaction);
-// });
+});
 
 client.on("message", msg => {
   console.log(msg);
@@ -29,16 +25,16 @@ client.on("message", msg => {
 
 client.login(process.env.BOT_TOKEN ?? "");
 
-async function getUser() {
-    try {
-        const response = await fetch('https://americas.api.riotgames.com/riot/account/v1/accounts/by-puuid/pKpJUczkiZbxotDYNAvs_29P35zuvZfuy5YxwDThQUj0098CBjRoNPY3--FiE-4rSb2IcibzS41S8Q', {
-        headers: {
-            "X-Riot-Token": "RGAPI-343501e1-64a9-4dac-b310-8f08129e7e1b"
-        }
-    })
-    let data = await response.json();
-    console.log(data)
-      } catch (error) {
-        console.error(error);
-      }
-}
+// async function getUser() {
+//     try {
+//         const response = await fetch('https://americas.api.riotgames.com/riot/account/v1/accounts/by-puuid/pKpJUczkiZbxotDYNAvs_29P35zuvZfuy5YxwDThQUj0098CBjRoNPY3--FiE-4rSb2IcibzS41S8Q', {
+//         headers: {
+//             "X-Riot-Token": ""
+//         }
+//     })
+//     let data = await response.json();
+//     console.log(data)
+//       } catch (error) {
+//         console.error(error);
+//       }
+// }
