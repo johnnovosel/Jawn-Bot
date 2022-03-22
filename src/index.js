3b142d12733f8cc6bb42f6a20864cc427ddcf159
johnnovosel <johnnovosel96@gmail.com>
2022-03-22 12:12:07 -0500

    fixed name

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ client.on("ready", async () => {
 });
 
 client.on("messageDelete", function(message) {
-	if (message.author.username != 'JawnBot') return;
+	if (message.author.username != 'JawnBotman') return;
 
 	let content = message.content;
 
